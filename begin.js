302a4e2aa75c676dfd8bf344db0ec2d19d72abc0
manob24 <wakilislam.24csedu.026@gmail.com>
2022-02-16 18:44:07 +0600

    init

---
+++ begin.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://rojnamcha:rojnamcha4321@cluster0.gfpd3.mongodb.net/Cluster0?retryWrites=true&w=majority&ssl=true'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, UseUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
